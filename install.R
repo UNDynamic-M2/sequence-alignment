52c6b69b520f6830d485960a1fe986794446535b
oss6 <ossedb@gmail.com>
2021-11-16 11:32:59 +0000

    fix install script for BEAR portal

--- install.R
+++ install.R
@@ -1,3 +1,17 @@
 print("Installing required packages...")
 deps = readLines("dependencies.txt")
+libs = .libPaths()
+
+conds = sapply(libs, function (l) { grepl("rds/homes", l, fixed = TRUE) })
+indices = which(conds == TRUE)
+priority_lib = NULL
+
+if (length(indices) > 0) {
+  priority_lib = indices[0]
+}
+
+if (!is.null(priority_lib)) {
+  .libPaths(c(priority_lib, libs))
+}
+
 install.packages(deps, repo="http://cran.rstudio.com/")
